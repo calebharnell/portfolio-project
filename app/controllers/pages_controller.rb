59686bf3676dffe88197d69d09ba71cc51e2bedb
calebharnell <calebharnell@hotmail.com>
2017-09-06 13:51:08 +1000

    Add meta tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,25 @@
 class PagesController < ApplicationController
   def index
+    @page_title       = 'Index'
+    @page_description = 'Portfolio Home page.'
+    @page_keywords    = 'Home, Index, Caleb Harnell, Portfolio'
   end
 
   def portfolio
+    @page_title       = 'Portfolio'
+    @page_description = 'Portfolio page'
+    @page_keywords    = 'Portfolio, Projects, Skills'
   end
 
   def about
+    @page_title       = 'About'
+    @page_description = 'About me page'
+    @page_keywords    = 'About, Work, Study, History'
   end
 
   def contact
+    @page_title       = 'Contact'
+    @page_description = 'Contact page'
+    @page_keywords    = 'Contact, Phone, Email'
   end
 end
