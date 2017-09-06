ca5bf7e40892000128b7a8412f53d9e127bf12fd
calebharnell <calebharnell@hotmail.com>
2017-09-06 15:40:14 +1000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#index'
   get '/index', { to: 'pages#index', as: 'index' }
   get '/portfolio', { to: 'pages#portfolio', as: 'portfolio' }
   get '/about', { to: 'pages#about', as: 'about' }
   get '/contact', { to: 'pages#contact', as: 'contact' }
+  root  :to => 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
